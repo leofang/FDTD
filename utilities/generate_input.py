7e84cf12ffc8e27425af77d7867edb789fb94ef5
Leo Fang <leofang@phy.duke.edu>
2016-10-21 00:10:39 -0400

    script updated

--- utilities/generate_input.py
+++ utilities/generate_input.py
@@ -8,20 +8,20 @@ from math import ceil
 ########################################
 
 ######### Simulation Parameters ########
-# nx = 2a/Delta
+# grid size
+Delta = 0.01
+# nx = 2a/Delta (in Delta)
 nx = 20
 # horizontal (half) box size (in qubit wavelength)
 Nx = 10
 # vertical box size (in qubit wavelength)
 Ny = 200
-# grid size
-Delta = 0.01
 
 ########## Physics Paramters ###########
 # qubit frequency (in Gamma)
-k0_n = 20
+k0 = 20
 # incident frequency (in Gamma)
-kn = 20
+k = 20
 # k0 a = n pi
 n = 0.5
 
@@ -42,9 +42,9 @@ Nx = ceil(Nx * nx/n)
 Ny = ceil(Ny * nx/n)
 
 # in units of Delta^-1
-k = (2.*n*pi/nx) * (kn/k0_n)
+k = (2.*n*pi/nx) * (k/k0)
 w0 = 2.*n*pi/nx
-Gamma = (2.*n*pi/nx)/k0_n
+Gamma = (2.*n*pi/nx)/k0
 
 # create the input file
 f = open(sys.argv[1], "w")
